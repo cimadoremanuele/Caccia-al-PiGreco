048179aa4e4f93c4646f5635e1b36c81957a4de8
Cimador Emanuele <emanuele.cimador@barsanti.edu.it>
2019-05-18 22:48:26 +0200

    inserimento commenti 18/5/19

--- Caccia_al_PiGreco/Caccia_al_PiGreco.ino
+++ Caccia_al_PiGreco/Caccia_al_PiGreco.ino
@@ -1,7 +1,5 @@
 #include <LiquidCrystal_I2C.h>
 
-String simbolo;
-int a; //attribuisco un valore da 0 a 6 a questa variabile con un random, per decidere se metto bonus, malus oppure niente
 int posizione;
 int tempo;
 int vite;
@@ -27,9 +25,7 @@ void setup() {
   btn3 = 11;
   btn4 = 12;
   btn5 = 13;
-  a = 0;
   corretto = false;
-  simbolo = "";
   lcd.init();
   lcd.backlight();
   pinMode(btnRecord, INPUT);
@@ -55,14 +51,12 @@ void loop() {
     PrimaRiga(vite, punti);
     while (vite > 0)
     {
-      var = 0;
-      corretto = false;
       SceltaPosizione();
-      if (a == 7)
+      if (random(1, 11) == 1)
       {
         malus();
       }
-      else if (a == 6)
+      else if (random(1, 11) == 1)
       {
         bonus();
       }
@@ -74,41 +68,38 @@ void loop() {
     PartitaFinita();
   }
   if (punti > record)
-  {
+  { //aggiorno il record se necessario
     record = punti;
   }
 }
-void Inizio()
+void Inizio() // scritta iniziale
 {
-  lcd.setCursor(0, 0);
-  lcd.print("Premi il bottone");
-  lcd.setCursor(0, 1);
-  lcd.print("blu per iniziare");
+  lcd.setCursor(0, 0); lcd.print("Premi il bottone");
+  lcd.setCursor(0, 1); lcd.print("blu per iniziare");
 }
-void Record()
+void Record() //scrivo il record
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Record : " + String(record));
+  lcd.setCursor(0, 0); lcd.print("Record : " + String(record));
   delay(4000);
   loop();
 }
-void SceltaPosizione()
+void SceltaPosizione() //scelgo
 {
+  var = 0;
+  corretto = false;
   delay(1500);
   int Array[] = {2, 5, 8, 11, 14};
-  posizione = random(0, 5);
-  lcd.setCursor(Array[posizione], 1);
-  a = random(0, 10);
+  posizione = random(0, 5); 
+  lcd.setCursor(Array[posizione], 1); //posizione dove scrivo il piGreco o il bonus o il malus
 }
 void malus()
 {
-  simbolo = "m"; //malus
-  lcd.print(simbolo);
+  lcd.print("m"); //scrivo il malus
   while (var < tempo)
   {
     if (BottoniErrati(0) == true)
-    {
+    { //l'utente ha premuto un bottone sbagliato
       vite--;
       tempo = tempo - 30;
       break;
@@ -121,19 +112,17 @@ void malus()
 }
 void bonus()
 {
-  simbolo = "b"; //bonus
-  lcd.print(simbolo);
+  lcd.print("b"); //scrivo il bonus
   while (var < tempo)
   {
-    int btn = posizione + 9;
-    if (digitalRead(btn) == HIGH)
-    {
+    if (digitalRead(posizione + 9) == HIGH)
+    { //l'utente ha premuto il bottone corretto
       vite++;
       tempo = tempo + 30;
       break;
     }
     else if (BottoniErrati(posizione + 9) == true)
-    {
+    { //l'utente ha premuto il bottone sbagliato
       vite--;
       tempo = tempo - 30;
       break;
@@ -144,30 +133,29 @@ void bonus()
   lcd.clear();
   PrimaRiga(vite, punti);
 }
+
 void piGreco()
 {
-  simbolo = "π"; //piGreco
-  lcd.print(simbolo);
+  lcd.print("π"); //scrivo il piGreco
   while (var < tempo)
   {
     if (digitalRead(posizione + 9) == HIGH)
-    {
+    { //l'utente ha premuto il bottone corretto
       punti++;
       tempo = tempo - 30;
       corretto = true;
       break;
     }
     else if (BottoniErrati(posizione + 9) == true)
-    {
+    { //l'utente ha premuto il bottone sbagliato
       corretto = false;
-      tempo = tempo - 30;
       break;
     }
     var++;
     delay(1);
   }
   if (corretto == false)
-  {
+  { //se l'utente ha sbagliato o non ha premuto nessun bottone diminuisco le vite e il tempo
     vite--;
     tempo = tempo - 30;
   }
@@ -181,7 +169,7 @@ void Azzera()
   var = 0;
   tempo = 1500;
 }
-bool BottoniErrati(int a)
+bool BottoniErrati(int a) //se l'utente preme il bottone sbagliato intervengo
 {
   bool sbagliato = false;
   for (int b = 9; b < 14; b++)
@@ -196,14 +184,14 @@ bool BottoniErrati(int a)
   }
   return sbagliato;
 }
-void PrimaRiga(int vita, int Punti)
+void PrimaRiga(int vita, int Punti) //scrivo vite e punti
 {
   lcd.setCursor(0, 0);
   lcd.print("Vite=" + String(vita));
   lcd.setCursor(8, 0);
   lcd.print("Punti=" + String(Punti));
 }
-void PartitaFinita()
+void PartitaFinita() //fine partita
 {
   lcd.clear();
   lcd.setCursor(0, 0);
